e6f6fd896a3d40de20b4aa5489cc2b7bdb61d7cd
Justin van der Hout <justinvdh@gmail.com>
2018-05-13 14:05:26 +0200

    Hacky and imperfect way to improve labeling

--- plugins/SerpentDasherGameAgentPlugin/files/serpent_Dasher_game_agent.py
+++ plugins/SerpentDasherGameAgentPlugin/files/serpent_Dasher_game_agent.py
@@ -4,15 +4,18 @@ import keyboard
 import os
 import _thread as thread
 import numpy as np
+import time
 from PIL import Image
 from datetime import datetime
 
 class SerpentDasherGameAgent(GameAgent):
     global timestamp
     global frame_count
+    global key_pressed
 
     timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S\\')
     frame_count = 0
+    key_pressed = False
 
     os.makedirs(os.path.dirname(os.getcwd() + "\\datasets\\"), exist_ok=True)
     os.makedirs(os.path.dirname(os.getcwd() + "\\datasets\\" + timestamp), exist_ok=True)
@@ -32,26 +35,34 @@ class SerpentDasherGameAgent(GameAgent):
 
     def handle_play(self, game_frame):
 
-        def save_game_frame(frame,name,print_message):
-            frame.save(name)
-            print(print_message)
+        def save_game_frame(frame,frame_cnt):
+            if not key_pressed:
+                time.sleep(0.03)
+                if not (key_pressed or old_key_pressed):
+                    frame.save("datasets\\" + timestamp + "\\no_jump\\" + str(frame_cnt) + ".png")
+                    print("Writing to no_jump")
+                else:
+                    frame.save("datasets\\" + timestamp + "\\jump\\" + str(frame_cnt) + ".png")
+                    print("Writing to jump")
+            else:
+                frame.save("datasets\\" + timestamp + "\\jump\\" + str(frame_cnt) + ".png")
+                print("Writing to jump")
 
         global timestamp
         global frame_count
+        global key_pressed
 
+        #Visual debugger
         for i, game_frame in enumerate(self.game_frame_buffer.frames):
             self.visual_debugger.store_image_data(
                 game_frame.grayscale_frame,
                 game_frame.grayscale_frame.shape,
                 str(i)
             )
+
         small_im = game_frame.eighth_resolution_frame
         gray_im = Image.fromarray(small_im).convert("L")
-
+        old_key_pressed = key_pressed
         key_pressed = keyboard.is_pressed('space')
-        if key_pressed:
-            thread.start_new_thread(save_game_frame,(gray_im,"datasets\\" + timestamp + "\\jump\\" + str(frame_count) + ".png","Writing to jump",))
-        else:
-            thread.start_new_thread(save_game_frame,(gray_im,"datasets\\" + timestamp + "\\no_jump\\"+ str(frame_count) + ".png","Writing to no_jump",))
-
+        thread.start_new_thread(save_game_frame,(gray_im,frame_count,))
         frame_count += 1
