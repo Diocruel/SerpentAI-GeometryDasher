90d85d373e33b0391aa658258b6e0a87beea3bef
Marissa van der Wel <m.m.vanderwel@student.tudelft.nl>
2018-05-15 17:26:34 +0200

    Structure for gameover frame removal. Still need to detect when gameover happens.

--- plugins/SerpentRecorderGameAgentPlugin/files/serpent_Recorder_game_agent.py
+++ plugins/SerpentRecorderGameAgentPlugin/files/serpent_Recorder_game_agent.py
@@ -7,12 +7,16 @@ import numpy as np
 import time
 from PIL import Image
 from datetime import datetime
-
+from serpent.sprite import Sprite
 class SerpentRecorderGameAgent(GameAgent):
     global timestamp
     global frame_count
     global key_pressed
-
+    global RemovedB
+    global removeFramesFilePath
+    
+    
+    RemovedB = False
     timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S\\')
     frame_count = 0
     key_pressed = False
@@ -22,7 +26,8 @@ class SerpentRecorderGameAgent(GameAgent):
     os.makedirs(os.path.dirname(os.getcwd() + "\\datasets\\" + timestamp + "\\jump\\"), exist_ok=True)
     os.makedirs(os.path.dirname(os.getcwd() + "\\datasets\\" + timestamp + "\\no_jump\\"), exist_ok=True)
     #open(os.getcwd() + "\\datasets\\" + timestamp + "presses.txt","w+")
-
+    removeFramesFilePath = os.getcwd()+"\\datasets\\remove\\"+timestamp[:-1]+".txt"
+    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -34,30 +39,55 @@ class SerpentRecorderGameAgent(GameAgent):
         pass
 
     def handle_play(self, game_frame):
-
-        def save_game_frame(frame,frame_cnt):
-            if not (key_pressed or old_key_pressed):
-                frame.save("datasets\\" + timestamp + "\\no_jump\\" + str(frame_cnt) + ".png")
-                print("Writing to no_jump")
-            else:
-                frame.save("datasets\\" + timestamp + "\\jump\\" + str(frame_cnt) + ".png")
-                print("Writing to jump")
-
-        global timestamp
+        #filename = os.getcwd() + "\\datasets\\collect_frames\\replay.png"
+        #image= Image.open(filename)
+        #image = np.array(image)
+        #image_data = image[...,np.newaxis]
+        #sprite = Sprite("GameOver", image_data=image_data)
+        #sprite.append_image_data(image_data)
+        #sprite_name = self.sprite_identifier.identify(sprite, mode="SIGNATURE_COLORS")
+        #if sprite_name != "UNKNOWN":
+        #    print(sprite_name)
+        
+        #insert method to check context
+        global RemovedB
         global frame_count
-        global key_pressed
-
-        #Visual debugger
-        for i, game_frame in enumerate(self.game_frame_buffer.frames):
-            self.visual_debugger.store_image_data(
-                game_frame.grayscale_frame,
-                game_frame.grayscale_frame.shape,
-                str(i)
-            )
-
-        small_im = game_frame.eighth_resolution_frame
-        gray_im = Image.fromarray(small_im).convert("L")
-        old_key_pressed = key_pressed
-        key_pressed = keyboard.is_pressed('space')
-        thread.start_new_thread(save_game_frame,(gray_im,frame_count,))
-        frame_count += 1
+        global timestamp
+        global removeFramesFilePath
+        if frame_count%20 != 0:
+            RemovedB = False
+            def save_game_frame(frame,frame_cnt):
+            
+                if not (key_pressed or old_key_pressed):
+                    frame.save("datasets\\" + timestamp + "\\no_jump\\" + str(frame_cnt) + ".png")
+                    #print("Writing to no_jump")
+                else:
+                    frame.save("datasets\\" + timestamp + "\\jump\\" + str(frame_cnt) + ".png")
+                    #print("Writing to jump")
+          
+            global key_pressed
+        
+            #Visual debugger
+            for i, game_frame in enumerate(self.game_frame_buffer.frames):
+                self.visual_debugger.store_image_data(
+                    game_frame.grayscale_frame,
+                    game_frame.grayscale_frame.shape,
+                    str(i)
+                )
+        
+            small_im = game_frame.eighth_resolution_frame
+            gray_im = Image.fromarray(small_im).convert("L")
+            old_key_pressed = key_pressed
+            key_pressed = keyboard.is_pressed('space')
+            thread.start_new_thread(save_game_frame,(gray_im,frame_count,))
+            frame_count += 1
+        else:
+            if not RemovedB:
+                RemovedB = True
+                print(frame_count)
+                removeFramesFile = open(removeFramesFilePath,"a+")
+                removeFramesFile.write(str(frame_count)+"\n")
+            #ONLY FOR TESTING SHOULD BE REMOVED LATER
+            frame_count +=1       
+                    
+        
---
+++ removeFrames.py
@@ -0,0 +1,24 @@
+import os
+directory = "./datasets/remove"
+removeBefore = 10
+for file in os.listdir(directory):
+    if file.endswith(".txt"):
+        timestamp = file
+        foldername = os.path.splitext(timestamp)[0]
+        with open(directory+"/"+timestamp) as fp:
+            for line in fp:
+                gameoverFrame = line.rstrip()
+                nr = int(gameoverFrame)
+                for framenr in range(max(0,nr-10),nr+1):                
+                    framenr = str(framenr) + ".png"
+                    jumpfullpath = "./datasets/"+foldername+"/jump/"+framenr
+                    nojumpfullpath = "./datasets/"+foldername+"/no_jump/"+framenr
+                    if os.path.isfile(jumpfullpath):
+                        os.remove(jumpfullpath)
+                        print(jumpfullpath+" deleted")
+                    elif os.path.isfile(nojumpfullpath):
+                        os.remove(nojumpfullpath)
+                        print(nojumpfullpath+" deleted")
+                    else:
+                        print("file does not exist")
+        #os.remove(directory+"/"+file)
