e835a043b28c33e3dcd0a2c45926d280153fb946
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2018-05-16 22:16:35 +0200

    Recorder Agent knows when it's game over

--- plugins/SerpentRecorderGameAgentPlugin/files/serpent_Recorder_game_agent.py
+++ plugins/SerpentRecorderGameAgentPlugin/files/serpent_Recorder_game_agent.py
@@ -7,6 +7,9 @@ import numpy as np
 import time
 from PIL import Image
 from datetime import datetime
+import sys
+sys.path.append(os.getcwd())
+from ImageNetwork import ImageNetwork
 
 class SerpentRecorderGameAgent(GameAgent):
     global timestamp
@@ -31,17 +34,30 @@ class SerpentRecorderGameAgent(GameAgent):
         self.frame_handler_setups["PLAY"] = self.setup_play
 
     def setup_play(self):
-        pass
+        context_classifier_path = f"datasets/context_classifier.model"
+
+        context_classifier = ImageNetwork(
+            input_shape=(60, 80, 3))  # Replace with the shape (rows, cols, channels) of your captured context frames
+
+        context_classifier.load_classifier(context_classifier_path)
+
+        self.machine_learning_models["context_classifier"] = context_classifier
 
     def handle_play(self, game_frame):
 
+        prediction = self.machine_learning_models["context_classifier"].predict(game_frame.frame)
+        if prediction == 1:
+            print("Game over :(")
+        else:
+            print("Still in it :)")
+        
         def save_game_frame(frame,frame_cnt):
             if not (key_pressed or old_key_pressed):
                 frame.save("datasets\\" + timestamp + "\\no_jump\\" + str(frame_cnt) + ".png")
-                print("Writing to no_jump")
+                #print("Writing to no_jump")
             else:
                 frame.save("datasets\\" + timestamp + "\\jump\\" + str(frame_cnt) + ".png")
-                print("Writing to jump")
+                #print("Writing to jump")
 
         global timestamp
         global frame_count
