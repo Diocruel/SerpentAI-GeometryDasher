47a0a0f02a73db7f2c91c68011604d3cc4a30b46
jannelie <J.N.deVries@student.tudelft.nl>
2018-05-28 15:20:02 +0200

    gameagent
    issues empty frames list, global frames

--- plugins/SerpentAudioGameAgentPlugin/files/serpent_Audio_game_agent.py
+++ plugins/SerpentAudioGameAgentPlugin/files/serpent_Audio_game_agent.py
@@ -1,15 +1,93 @@
 from serpent.game_agent import GameAgent
+from collections import deque
+import keyboard
+import os
+import _thread as thread
+import numpy as np
+import time
+import multiprocessing
+import pyaudio
+import wave
+from AudioNetwork import AudioNetwork
 
+ 
+def record(frames):
+    global FORMAT
+    global defaultframes
 
+    defaultframes = 512
+    FORMAT = pyaudio.paInt16
+    device_info = {}
+    useloopback = False
+    
+    # Use module
+    p = pyaudio.PyAudio()
+    
+    # Get input or default
+    device_id = 5
+    print("")
+    
+    # Get device info
+    try:
+        device_info = p.get_device_info_by_index(device_id)
+    except IOError:
+        print("Couldn't use audio device, please set correct device_id")
+        exit()
+    
+    # Choose between loopback or standard mode
+    is_input = device_info["maxInputChannels"] > 0
+    is_wasapi = (p.get_host_api_info_by_index(device_info["hostApi"])["name"]).find("WASAPI") != -1
+    if is_input:
+        print("Selection is input using standard mode.\n")
+    else:
+        if is_wasapi:
+            useloopback = True
+            print("Selection is output. Using loopback mode.\n")
+        else:
+            print("Selection is output and does not support loopback mode. Quitting.\n")
+            exit()
+    
+    # recordtime = int(input("Record time in seconds [" + str(recordtime) +"]: ") or recordtime)
+    
+    # Open stream
+    channelcount = device_info["maxInputChannels"] if (
+        device_info["maxOutputChannels"] < device_info["maxInputChannels"]) else device_info["maxOutputChannels"]
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=channelcount,
+                    rate=int(device_info["defaultSampleRate"]),
+                    input=True,
+                    frames_per_buffer=defaultframes,
+                    input_device_index=device_info["index"],
+                    as_loopback=useloopback)
+    
+    print("recording...")
+
+    while not keyboard.is_pressed('q'):
+        stream.start_stream()
+        #start_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f')
+        #for i in range(0, int(int(device_info["defaultSampleRate"]) / defaultframes * recordtime)):
+        frames.append(stream.read(defaultframes))
+       # print(list(frames))
+    
+    
+    # stop Recording
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+        
 class SerpentAudioGameAgent(GameAgent):
-
+    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
+        global frames
+        frames = deque([],maxlen=100)
+        audio_thread = multiprocessing.Process(target=record, args=(frames,))
+        audio_thread.start()
         self.frame_handlers["PLAY"] = self.handle_play
 
         self.frame_handler_setups["PLAY"] = self.setup_play
-
+       
+    
     def setup_play(self):
         classifier_path = f"datasets/pretrained_audio_classifier.model"
 
@@ -21,12 +99,13 @@ class SerpentAudioGameAgent(GameAgent):
         self.machine_learning_models["classifier"] = classifier
 
     def handle_play(self, game_frame):
-		
-		audioframe = 
-		
-        start = time()
-        prediction = self.machine_learning_models["classifier"].predict(audioframe)
-        end = time()
-        if (prediction == 1) :
-            self.input_controller.tap_key(KeyboardKey.KEY_UP)
-        print("time : " + str(end-start))
+        global frames
+        audioframe = list(frames)
+        print(audioframe)
+        
+        #start = time()
+        #prediction = self.machine_learning_models["classifier"].predict(audioframe)
+        #end = time()
+        #if (prediction == 1) :
+        #    self.input_controller.tap_key(KeyboardKey.KEY_UP)
+        #print("time : " + str(end-start))
